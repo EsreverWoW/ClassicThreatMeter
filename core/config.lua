b39c54a78668162c01101f4dc23287c2248e97d7
EsreverWoW <49285632+EsreverWoW@users.noreply.github.com>
2019-08-28 03:29:52 -0500

    Faster update default

--- core/config.lua
+++ core/config.lua
@@ -11,7 +11,7 @@ A.defaultConfig = {}
 -- general
 A.defaultConfig.general = {
 	welcome				= true,									-- display welcome message
-	update				= 0.25,									-- time (in seconds) between updates
+	update				= 0.1,									-- time (in seconds) between updates
 	minimap				= false,								-- toggle for minimap icon
 	ignorePets			= false,								-- toggle for ignoring pets
 	hideOOC				= false,								-- hide frame when out of combat
