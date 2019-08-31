83018a8bff7cbd62236505d9da5759fd8e9ed002
EsreverWoW <49285632+EsreverWoW@users.noreply.github.com>
2019-08-31 04:17:55 -0500

    temporary safeguard for now

--- core/core.lua
+++ core/core.lua
@@ -222,6 +222,7 @@ local function UpdateThreatBars()
 		local data = threatData[i]
 		local bar = CTM.bars[i]
 		if data and data.threatValue > 0 then
+			if bar == CTM.bars[1] then data.scaledPercent = 100 end -- temporary?
 			bar.name:SetText(UnitName(data.unit) or UNKNOWN)
 			bar.val:SetText(NumFormat(data.threatValue))
 			bar.perc:SetText(floor(data.scaledPercent).."%")
